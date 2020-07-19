a149e1f90d0b3d941a2d98bc17df8b2703d7d782
Alex <revolalex@gmail.com>
2020-07-19 16:52:33 +0200

    using Promise.all

--- app2.js
+++ app2.js
@@ -2,22 +2,24 @@ const MongoClient = require("mongodb").MongoClient;
 const fetch = require("node-fetch");
 
 const URL = "mongodb://localhost:27017";
-const URL_API = "https://restcountries.eu/rest/v2/name/";
+const API_URL = "https://restcountries.eu/rest/v2/name/";
 
 const main = async () => {
   const client = await MongoClient.connect(URL, { useUnifiedTopology: true });
   const dataBase = client.db("Country");
 
-  const country = await dataBase.collection("country_names").find({}).toArray();
-  
+  const countries = await dataBase
+    .collection("country_names")
+    .find({})
+    .toArray();
+
   try {
-    console.log("This process will take arround 40s...");
-    for (i = 0; i < country.length; i++) {
-      let response = await fetch(URL_API + country[i].name);
-      let pays = await response.json();
-      // question to ask choose "insert" or "insertMany"
-      dataBase.collection("country_full_data").insertMany(pays);
-    }
+    const datas = await Promise.all(fecthCountriesData(countries));
+    //  create an array that don't contains another array
+    let data = datas.map((element) => element[0]);
+    
+    dataBase.collection("country_full_data").insertMany(data);
+
   } catch (e) {
     console.log(e);
   } finally {
@@ -27,3 +29,17 @@ const main = async () => {
 };
 
 main();
+/**
+ * @summary loop through all the countries and fetch all the information, then push them in an array
+ * @param {*} countriesList 
+ * @returns an array containing all the countries with full data
+ */
+let fecthCountriesData = (countriesList) => {
+  let countriesWithData = [];
+  for (country of countriesList) {
+    countriesWithData.push(
+      fetch(API_URL + country.name).then((res) => res.json())
+    );
+  }
+  return countriesWithData;
+};
---
+++ app2Bis.js
@@ -0,0 +1,30 @@
+// without using Promise.all
+const MongoClient = require("mongodb").MongoClient;
+const fetch = require("node-fetch");
+
+const URL = "mongodb://localhost:27017";
+const URL_API = "https://restcountries.eu/rest/v2/name/";
+
+const main = async () => {
+  const client = await MongoClient.connect(URL, { useUnifiedTopology: true });
+  const dataBase = client.db("Country");
+
+  const country = await dataBase.collection("country_names").find({}).toArray();
+  
+  try {
+    console.log("This process will take arround 40s...");
+    for (i = 0; i < country.length; i++) {
+      let response = await fetch(URL_API + country[i].name);
+      let pays = await response.json();
+      // question to ask choose "insert" or "insertMany"
+      dataBase.collection("country_full_data").insertMany(pays);
+    }
+  } catch (e) {
+    console.log(e);
+  } finally {
+    console.log("!==>  Succes Country Full Data has been created  <==!");
+    client.close();
+  }
+};
+
+main();
